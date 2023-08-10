7c20a4e8267ba9dd486c7dea40b7e4946a68dd35
royzkie24 <carrerachristianroy@gmail.com>
2023-08-10 16:33:20 +0800

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,25 @@
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres } = useGenres();
+  return (
+    <>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default GenreList;
