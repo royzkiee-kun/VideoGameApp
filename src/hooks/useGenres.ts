eaa5c3502d2c2fbd60cc230c37d46171f1ee2762
Roy <rcarrera@topjuan-tech.com>
2024-05-29 15:58:28 +0800

    Install tanstack react query
    Create tanstack react query function to fetch genre list from api

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,8 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import useData, { FetchResponse } from "./useData";
+import genres from "../data/genres";
+import apiClient from "../services/api-client";
+
 
 export type Genre = {
   id: number;
@@ -6,6 +10,16 @@ export type Genre = {
   image_background: string;
 };
 
-const useGenres = () => useData<Genre>("/genres");
+// const useGenres = () => useData<Genre>("/genres");
+
+const useGenres = () => useQuery({
+  queryKey: ['genres'],
+  queryFn: () =>
+     apiClient
+  .get<FetchResponse<Genre>>(`/genres`)
+  .then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, //24hrs
+  initialData: {count : genres.length, results: genres}
+})
 
 export default useGenres;
